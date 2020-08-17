7c75b7f0eccff30b1841ffc541eedd641cb218f0
sunilkumarmt <41679312+sunilkumarmt@users.noreply.github.com>
2020-08-17 09:31:46 +0530

    added a new file: Temp.cs

---
+++ Test2/Test2/Temp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    class Temp
+    {
+        public void test()
+        {
+            Console.WriteLine("inside the method");
+        }
+    }
+}
